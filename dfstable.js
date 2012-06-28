313e0d26841bcfc42f010090a2fb738e8e1142bc
hossein <hosseamin@gmail.com>
2012-06-29 01:21:25 +0430

    dfstable extension

--- dfstable.js
+++ dfstable.js
@@ -1,4 +1,21 @@
 /*
+ * Visual Table does simple table look by positioning his cells
+ *   Copyright (C) 2012  Hossein Amin
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
   Directional Fixed Size(uniform cells) Table
   is an abstraction on vtable which manages sizes and cell positioning
   in specific direction
